cb0b2f4868e8f8c540428e8dcd162b99230d3f9f
NgocAnh <ngocanh411997@gmail.com>
2018-04-15 02:27:11 +0700

    [Ánh] sửa lại CSDL

---
+++ [Ánh].sql
@@ -0,0 +1,16 @@
+﻿-- Sửa CSDL
+go
+DROP TABLE dbo.PhieuDichVu
+-- Thêm bảng PhieuDichVu và DichVu
+GO 
+CREATE TABLE DichVu(
+MaDV varchar(10) PRIMARY KEY ,
+TenDV nvarchar(50),
+Gia bigint)
+
+go
+CREATE TABLE PhieuDichVu(
+MaPhieuDK VARCHAR(10) REFERENCES dbo.PhieuDangKy(MaPhieuDK),
+MaDV VARCHAR(10) REFERENCES dbo.DichVu(MaDV),
+SoLuong INT,
+PRIMARY KEY(MaPhieuDK, MaDV))
