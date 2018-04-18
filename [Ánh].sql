2b5ef9438a9b85a8ae6d9203644d04224b2aaf97
NgocAnh <ngocanh411997@gmail.com>
2018-04-18 13:16:24 +0700

    [Ánh] Thủ tục

--- [Ánh].sql
+++ [Ánh].sql
@@ -15,3 +15,39 @@ MaPhieuDK VARCHAR(10) REFERENCES dbo.PhieuDangKy(MaPhieuDK),
 MaDV VARCHAR(10) REFERENCES dbo.DichVu(MaDV),
 SoLuong INT,
 PRIMARY KEY(MaPhieuDK, MaDV))
+
+---- Thủ tục DỊCH VỤ
+-- Xem dịch vụ
+GO
+CREATE PROC XemDV
+AS
+BEGIN
+	SELECT *
+	FROM dbo.DichVu
+END
+-- Thêm dịch vụ
+GO
+CREATE PROC ThemDV(@MaDV VARCHAR(10), @TenDV NVARCHAR(50),@Gia BIGINT)
+AS
+BEGIN
+	INSERT INTO dbo.DichVu
+	        ( MaDV, TenDV, Gia )
+	VALUES  ( @MaDV,@TenDV,@Gia)
+END
+-- Sửa dịch vụ
+GO
+CREATE PROC SuaDV(@MaDV VARCHAR(10), @TenDV NVARCHAR(50),@Gia BIGINT)
+AS
+BEGIN
+	UPDATE dbo.DichVu
+	SET TenDV=@TenDV,Gia=@Gia
+	WHERE MaDV=@MaDV
+END
+-- Xóa dịch vụ
+GO
+CREATE PROC XoaDV(@MaDV VARCHAR(10))
+AS
+BEGIN
+	DELETE dbo.DichVu
+	WHERE MaDV=@MaDV
+END
