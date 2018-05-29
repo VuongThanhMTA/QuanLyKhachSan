1bb6e9b35e1da28334bbd895da092c0e75d45535
HongSonMTA <36963575+HongSonMTA@users.noreply.github.com>
2018-05-29 23:32:45 +0700

    [Sơn] Insert Dữ Liệu Nhân Viên

---
+++ insertData[Sơn].sql
@@ -0,0 +1,32 @@
+﻿USE QuanLyKhachSan
+GO
+INSERT INTO dbo.NhanVien
+        ( MaNV , TenNV , GioiTinh ,NgaySinh ,DiaChi ,CMND , SDT , Luong)
+VALUES  ( 'NV01' ,N'Nguyễn Hồng Sơn' ,N'Nam' , '12-04-1997' , N'Hà Nội' ,'145679222' ,'019998888' , 10000000 )
+INSERT INTO dbo.NhanVien
+        ( MaNV , TenNV , GioiTinh ,NgaySinh ,DiaChi ,CMND , SDT , Luong)
+VALUES  ( 'NV02' ,N'Nguyễn Văn Tính' ,N'Nam' , '01-05-1993' , N'Hà Nội' ,'145679332' ,'019998888' , 9000000 )
+INSERT INTO dbo.NhanVien
+        ( MaNV , TenNV , GioiTinh ,NgaySinh ,DiaChi ,CMND , SDT , Luong)
+VALUES  ( 'NV03' ,N'Nguyễn Thị Ngọc Ánh' ,N'Nữ' , '12-06-1997' , N'Hà Nam' ,'145655222' ,'019998888' , 11000000 )
+INSERT INTO dbo.NhanVien
+        ( MaNV , TenNV , GioiTinh ,NgaySinh ,DiaChi ,CMND , SDT , Luong)
+VALUES  ( 'NV04' ,N'Trần Văn Hiếu' ,N'Nam' , '12-01-1992' , N'Hà Tĩnh' ,'145679777' ,'019998888' , 12000000 )
+INSERT INTO dbo.NhanVien
+        ( MaNV , TenNV , GioiTinh ,NgaySinh ,DiaChi ,CMND , SDT , Luong)
+VALUES  ( 'NV05' ,N'Vương Văn Thanh' ,N'Nam' , '12-26-1992' , N'Hà Nội' ,'145679882' ,'019998888' , 15000000 )
+INSERT INTO dbo.NhanVien
+        ( MaNV , TenNV , GioiTinh ,NgaySinh ,DiaChi ,CMND , SDT , Luong)
+VALUES  ( 'NV06' ,N'Phạm Quang Minh' ,N'Nam' , '12-04-1994' , N'Quảng Ninh' ,'145672222' ,'019998888' , 11000000 )
+INSERT INTO dbo.NhanVien
+        ( MaNV , TenNV , GioiTinh ,NgaySinh ,DiaChi ,CMND , SDT , Luong)
+VALUES  ( 'NV07' ,N'Phan Duy Dương' ,N'Nam' , '12-04-1995' , N'Hà Nội' ,'145679222' ,'019998888' , 12000000 )
+INSERT INTO dbo.NhanVien
+        ( MaNV , TenNV , GioiTinh ,NgaySinh ,DiaChi ,CMND , SDT , Luong)
+VALUES  ( 'NV08' ,N'Nguyễn Văn Tèo' ,N'Nam' , '12-04-1998' , N'Hà Nội' ,'145679222' ,'019998888' , 14000000 )
+INSERT INTO dbo.NhanVien
+        ( MaNV , TenNV , GioiTinh ,NgaySinh ,DiaChi ,CMND , SDT , Luong)
+VALUES  ( 'NV09' ,N'Hồ Quang Hiếu' ,N'Nam' , '12-04-1991' , N'Hà Nội' ,'145679222' ,'019998888' , 10000000 )
+INSERT INTO dbo.NhanVien
+        ( MaNV , TenNV , GioiTinh ,NgaySinh ,DiaChi ,CMND , SDT , Luong)
+VALUES  ( 'NV10' ,N'Nguyễn Duy Mạnh' ,N'Nam' , '12-04-1990' , N'Hà Nội' ,'145679222' ,'019998888' , 10000000 )
